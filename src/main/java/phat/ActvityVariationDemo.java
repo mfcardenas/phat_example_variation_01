1acb20f3594cbbeeada2db3d2b03e881a82a7d13
mfcardenas <marlonca@ucm.es>
2018-02-19 17:53:15 +0100

    add example

--- src/main/java/phat/ActvityVariationDemo.java
+++ src/main/java/phat/ActvityVariationDemo.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2014 Pablo Campillo-Sanchez <pabcampi@ucm.es>
  *
- * This software has been developed as part of the
+ * This software has been developed as part of the 
  * SociAAL project directed by Jorge J. Gomez Sanz
  * (http://grasia.fdi.ucm.es/sociaal)
  *
@@ -18,7 +18,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package phat;
-import java.util.logging.Logger;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
@@ -27,188 +26,242 @@ import com.jme3.app.state.AppStateManager;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-
+import com.jme3.scene.Node;
+import phat.agents.actors.parkinson.HandTremblingControl;
 import phat.app.PHATApplication;
 import phat.app.PHATInitAppListener;
+import phat.beans.Body;
+import phat.beans.PhatSimulationBean;
 import phat.body.BodiesAppState;
-import phat.body.commands.PlayBodyAnimationCommand;
-import phat.body.commands.SetBodyInCoordenatesCommand;
-import phat.body.commands.SetSpeedDisplacemenetCommand;
+import phat.body.commands.AlignWithCommand;
+import phat.body.commands.CloseObjectCommand;
+import phat.body.commands.GoCloseToObjectCommand;
+import phat.body.commands.OpenObjectCommand;
+import phat.commands.MovArmCommand;
+import phat.commands.PHATCommand;
+import phat.commands.PHATCommandListener;
 import phat.devices.DevicesAppState;
-import phat.devices.commands.CreateAccelerometerSensorCommand;
+import phat.devices.commands.CreateSmartphoneCommand;
+import phat.devices.commands.SetDeviceOnFurnitureCommand;
 import phat.devices.commands.SetDeviceOnPartOfBodyCommand;
 import phat.environment.SpatialEnvironmentAPI;
 import phat.sensors.accelerometer.AccelerometerControl;
 import phat.sensors.accelerometer.XYAccelerationsChart;
 import phat.server.ServerAppState;
-import phat.server.commands.ActivateAccelerometerServerCommand;
-import phat.structures.houses.HouseFactory;
 import phat.structures.houses.commands.CreateHouseCommand;
+import phat.utils.ReadJSON;
+import phat.utils.Utils;
 import phat.world.WorldAppState;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.util.logging.Logger;
 
 /**
  * Class example Test rum simulatios.
- * <br/>
- * Interval Execute Classificator<br/>
- * float timeToChange = 10f;<br/>
- * ...<br/>
- * if (cont > timeToChange && cont < timeToChange + 1 && !fall) {<br/>
- * ...<br/>
- * if (fall && cont > timeToChange + 10) {<br/>
- * <br/>
- * Interval Execute Capture Data<br/>
- * float timeToChange = 2f;<br/>
- * ...<br/>
- * if (cont > timeToChange && cont < timeToChange + 1 && !fall) {<br/>
- * ...<br/>
- * if (fall && cont > timeToChange + 2) {<br/>
- * <br/>
- * Optimal min distance between animation<br/>
- * 0.1 * 0.1 * 0.2 <br/>
- * <br/>
- * <b>Gesture</b>
- * SpinSpindle: 	abrir puerta con dificultad<br/>
- * Hands2Hips: 		llevar manos a la cadera, (dolor de espalda)<br/>
- * Hand2Belly: 		llevar la mano al vientre, (dolor de vientre)<br/>
- * Wave: 			pedir ayuda o llamar atención<br/>
- * ScratchArm: 		rascar el codo<br/>
- * LeverPole: 		molestias en el movimiento y pedir ayuda   <br/>
  * @author UCM
  */
-public class ActvityVariationDemo implements PHATInitAppListener {
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class ActvityVariationDemo implements PHATInitAppListener{
 
     private static final Logger logger = Logger.getLogger(ActvityVariationDemo.class.getName());
+    
+    /**
+     * Phat Simulation Object.
+     */
+    PhatSimulationBean phatSimulation;
+    
+    /**
+     * Bodies App State.
+     */
     BodiesAppState bodiesAppState;
-    ServerAppState serverAppState;
     DevicesAppState devicesAppState;
     WorldAppState worldAppState;
-
-
-    @Override
+    ServerAppState serverAppState;
+    
     public void init(SimpleApplication app) {
-        logger.info("init");
-        AppStateManager stateManager = app.getStateManager();
+    	
+    	String fileConfig = "config_simulation_v3.json";
+    	
+    	ScriptEngineManager manager = new ScriptEngineManager();
+    	ScriptEngine engine = manager.getEngineByName("Renjin");
 
+    	AppStateManager stateManager = app.getStateManager();
         app.getFlyByCamera().setMoveSpeed(10f);
 
         BulletAppState bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
-        bulletAppState.setDebugEnabled(false);
-
+        
         SpatialEnvironmentAPI seAPI = SpatialEnvironmentAPI.createSpatialEnvironmentAPI(app);
-
-        seAPI.getWorldAppState().setCalendar(2016, 6 ,1, 12, 30, 0);
-        seAPI.getHouseAppState().runCommand(new CreateHouseCommand("House1", HouseFactory.HouseType.House3room2bath));
-
+        seAPI.getWorldAppState().setCalendar(2016, 2 ,18, 12, 30, 0);
+                
+        setPhatSimulation(ReadJSON.initPhatSimConfig(fileConfig));
+        
+        bulletAppState.setDebugEnabled(phatSimulation.isDebug());
+        seAPI.getHouseAppState().runCommand(new CreateHouseCommand(phatSimulation.getNameHouse(), Utils.getTypeHouse(phatSimulation.getTypeHouse())) );
+                	
         bodiesAppState = new BodiesAppState();
         stateManager.attach(bodiesAppState);
-
-        //Se crean los personajes
-        bodiesAppState.createBody(BodiesAppState.BodyType.Young, "Patient");
-
-        //Se posicionan en la casa
-        bodiesAppState.setInSpace("Patient", "House1", "Chair3");
-
-        bodiesAppState.runCommand(new SetBodyInCoordenatesCommand("Patient", Vector3f.ZERO));
-        bodiesAppState.runCommand(new SetSpeedDisplacemenetCommand("Patient", 0.5f));
-
+        
+        //Se crean los personajes y se posicionan en el espacio
+        for(Body body: phatSimulation.getBodies()){
+            bodiesAppState.createBody(Utils.getTypeBody(body.getType()), body.getName());
+            bodiesAppState.setInSpace(body.getName(), phatSimulation.getNameHouse(), body.getPosition());
+        }
+        
         devicesAppState = new DevicesAppState();
         stateManager.attach(devicesAppState);
-
-        // Positions of sensor Acelerometers
-        devicesAppState.runCommand(new CreateAccelerometerSensorCommand("sensor1"));
-        devicesAppState.runCommand(new SetDeviceOnPartOfBodyCommand("Patient", "sensor1",
-                SetDeviceOnPartOfBodyCommand.PartOfBody.LeftHand));
-
-        devicesAppState.runCommand(new CreateAccelerometerSensorCommand("sensor2"));
-        devicesAppState.runCommand(new SetDeviceOnPartOfBodyCommand("Patient", "sensor2",
-                SetDeviceOnPartOfBodyCommand.PartOfBody.RightHand));
-
-        devicesAppState.runCommand(new CreateAccelerometerSensorCommand("sensor3"));
-        devicesAppState.runCommand(new CreateAccelerometerSensorCommand("sensor4"));
-
-        serverAppState = new ServerAppState();
-        stateManager.attach(serverAppState);
-
-        // Activate Acelerometers
-        serverAppState.runCommand(new ActivateAccelerometerServerCommand("PatientBodyAccel", "sensor1"));
-        serverAppState.runCommand(new ActivateAccelerometerServerCommand("PatientBodyAccel", "sensor2"));
-
-        app.getCamera().setLocation(new Vector3f(6.5472484f, 1.5681753f, 4.951794f));
-        app.getCamera().setRotation(new Quaternion(0.030681776f, 0.9156617f, -0.0712238f, 0.39439762f));
-
+        
+        devicesAppState.runCommand(new CreateSmartphoneCommand("SmartWatch1").setDimensions(0.03f, 0.03f, 0.01f));
+        devicesAppState.runCommand(new SetDeviceOnPartOfBodyCommand("Patient1", "SmartWatch1", 
+                SetDeviceOnPartOfBodyCommand.PartOfBody.LeftWrist));
+        
+        devicesAppState.runCommand(new CreateSmartphoneCommand("SmartWatch2").setDimensions(0.03f, 0.03f, 0.01f));
+        devicesAppState.runCommand(new SetDeviceOnPartOfBodyCommand("Patient1", "SmartWatch2", 
+                SetDeviceOnPartOfBodyCommand.PartOfBody.LeftWrist));
+        
+        devicesAppState.runCommand(new SetDeviceOnFurnitureCommand("deviceId", "House1", "furnitureId"));
+        
         stateManager.attach(new AbstractAppState() {
-            PHATApplication app;
 
-            @Override
-            public void initialize(AppStateManager asm, Application aplctn) {
-                app = (PHATApplication) aplctn;
-
-            }
-
-            float cont = 0f;
-            boolean fall = false;
-            float timeToChange = 10f;
+        	@Override
+            public void initialize(AppStateManager asm, Application aplctn) {}
+            
             boolean init = false;
-            boolean traindata = false;
 
             @Override
             public void update(float f) {
-                if (!init && !traindata) {
-                    // Grafica del Sensor2
-                    AccelerometerControl ac2 = devicesAppState.getDevice("sensor2")
-                            .getControl(AccelerometerControl.class);
-                    ac2.setMode(AccelerometerControl.AMode.GRAVITY_MODE);
-                    XYAccelerationsChart chart2 = new XYAccelerationsChart("Data Accelerations Rigth Hand", "Local Sensor PHAT-SIM Rigth Hand", "m/s2",
-                            "x,y,z");
-                    ac2.add(chart2);
-                    chart2.showWindow();
-                    init = true;
+            	if(!init){
+            		AccelerometerControl ac = devicesAppState.getDevice("SmartWatch1").getControl(AccelerometerControl.class);
+                    ac.setMode(AccelerometerControl.AMode.ACCELEROMETER_MODE);
+                    XYAccelerationsChart chart = new XYAccelerationsChart("Chart - Accelometer Mode", "Smartphone 1 Mov", "m/s2", "x,y,z");
+                    ac.add(chart);
+                    chart.showWindow();
+                    init = true;	
+            	}
+            }
+        });
+        
+        stateManager.attach(new AbstractAppState() {
 
-                    // Grafica del Sensor1
-                    AccelerometerControl ac1 = devicesAppState.getDevice("sensor1")
-                            .getControl(AccelerometerControl.class);
-                    ac1.setMode(AccelerometerControl.AMode.GRAVITY_MODE);
-                    XYAccelerationsChart chart1 = new XYAccelerationsChart("Data Accelerations Left Hand", "Local Sensor PHAT-SIM Left Hand", "m/s2",
-                            "x,y,z");
-                    ac1.add(chart1);
-                    chart1.showWindow();
-                    init = true;
+        	@Override
+            public void initialize(AppStateManager asm, Application aplctn) {}
+            
+            boolean init = false;
 
-                    // Grafica del Sensor...
-
-                }
-
-                cont += f;
-                if (cont > timeToChange && cont < timeToChange + 1f && !fall) {
-                    System.out.println("Change to DrinkStanding:::" + String.valueOf(cont) + "-" + String.valueOf(f));
-                    bodiesAppState.runCommand(new PlayBodyAnimationCommand("Patient", "DrinkStanding"));
-                    fall = true;
-                } else {
-                    if (fall && cont > timeToChange + 10f) {
-                        System.out.println("Change to WaveAttention:::" + String.valueOf(cont) + "-" + String.valueOf(f));
-                        bodiesAppState.runCommand(new PlayBodyAnimationCommand("Patient", "WaveAttention"));
-                        fall = false;
-                        cont = 0;
-                    }
-                }
+            @Override
+            public void update(float f) {
+            	if(!init){
+            		AccelerometerControl ac = devicesAppState.getDevice("SmartWatch2").getControl(AccelerometerControl.class);
+                    ac.setMode(AccelerometerControl.AMode.GRAVITY_MODE);
+                    XYAccelerationsChart chart = new XYAccelerationsChart("Chart - Gravity Mode", "Smartphone 2 Mov", "m/s2", "x,y,z");
+                    ac.add(chart);
+                    chart.showWindow();
+                    init = true;	
+            	}
             }
         });
 
+        /*phatSimulation.setCommands(ReadJSON.initPhatSimCommands(url));
+        if(phatSimulation.getCommands() != null && phatSimulation.getCommands().size() > 0){
+        	for(Object obj: phatSimulation.getCommands().values()){
+        		bodiesAppState.runCommand((PHATCommand) obj);
+        	}
+        }*/
 
-
+        openObject("Patient","Fridge1");
+        goCloseObjectSane("Patient1", new String[]{"Fridge1"}, false);
+        app.getCamera().setLocation(new Vector3f(7f, 7.25f, 6.1f));
+        app.getCamera().setRotation(new Quaternion(0.37133554f, -0.6016627f, 0.37115145f, 0.60196227f));
     }
 
     /**
+     * Go to Close Object Sane.
+     * @param source source
+     * @param target target
+     * @param forget forget
+     */
+    private void goCloseObjectSane(final String source, final String[] target, final boolean forget) {
+    	logger.info("goToClose: " + source + ", object: " + target);
+        GoCloseToObjectCommand gtc = new GoCloseToObjectCommand(source, target[0], new PHATCommandListener() {
+        	
+            @Override
+            public void commandStateChanged(PHATCommand command) {
+
+            	if (command.getState() == PHATCommand.State.Success) {
+                	bodiesAppState.runCommand(new MovArmCommand(source, false, MovArmCommand.LEFT_ARM));
+                	bodiesAppState.runCommand(new AlignWithCommand(source, target[0]));
+                	if(!forget){
+                		bodiesAppState.runCommand(new CloseObjectCommand(source, target[0]));
+                	}
+                	bodiesAppState.runCommand(new MovArmCommand(source,true, MovArmCommand.LEFT_ARM));
+                }
+                
+                if(command.getState().equals(PHATCommand.State.Running)){
+                	
+                }
+            }
+        });
+        gtc.setMinDistance(0.1f);
+        bodiesAppState.runCommand(gtc);
+    }
+    
+    private void goToCrazy(final String source, final String target){
+    	logger.info("--> GoCrazy from source: " + source + " to " + target);
+    	GoCloseToObjectCommand gtc = new GoCloseToObjectCommand(source, target, new PHATCommandListener() {
+    		
+            @Override
+            public void commandStateChanged(PHATCommand command) {
+                if (command.getState() == PHATCommand.State.Running) {
+                	Node body = bodiesAppState.getBodiesNode();
+                	HandTremblingControl htcR = new HandTremblingControl(HandTremblingControl.Hand.RIGHT_HAND);
+                    body.addControl(htcR);  
+                    HandTremblingControl htcL = new HandTremblingControl(HandTremblingControl.Hand.LEFT_HAND);
+                    body.addControl(htcL);
+                }
+            }
+        });
+        gtc.setMinDistance(0.1f);
+        bodiesAppState.runCommand(gtc);
+    }
+    
+    /**
+     * Open Door Actions.
+     * @param source
+     * @param target 
+     */
+    private void openObject(final String source, final String target) {
+        OpenObjectCommand gtc = new OpenObjectCommand(source, target);
+        bodiesAppState.runCommand(gtc);
+    }
+    
+    /**
      * Main Class, executios Test.
-     * @param args 0
+     * @param args 
      */
     public static void main(String[] args) {
         ActvityVariationDemo app = new ActvityVariationDemo();
         PHATApplication phat = new PHATApplication(app);
+        
         phat.setDisplayFps(false);
         phat.setDisplayStatView(false);
         phat.setShowSettings(false);
+        
         phat.start();
     }
+
+	/**
+	 * @return the phatSimulation
+	 */
+	public PhatSimulationBean getPhatSimulation() {
+		return phatSimulation;
+	}
+
+	/**
+	 * @param phatSimulation the phatSimulation to set
+	 */
+	public void setPhatSimulation(PhatSimulationBean phatSimulation) {
+		this.phatSimulation = phatSimulation;
+	}
+   
 }
